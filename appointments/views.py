cd087ebaeb47a7a0c306ce09afe70880089dc2f1
Krishnamurthy Chandran <x23380616@student.ncirl.ie>
2025-04-05 16:28:59 +0100

    Trigger CI/CD with test comment

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,4 @@
+# CI/CD test deployment run ✅
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
